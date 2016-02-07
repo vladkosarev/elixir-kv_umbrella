898c9fb39b260b99a673e9744a3d73173764a519
vladkosarev <vladk@om-tech.net>
2016-02-06 19:49:29 -0500

    Initial structure

---
+++ apps/kv/test/bucket_test.exs
@@ -0,0 +1,22 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  test "stores values by key" do
+    {:ok, bucket} = KV.Bucket.start_link
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+  
+  test "deletes values by key" do
+    {:ok, bucket} = KV.Bucket.start_link
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+    
+    KV.Bucket.delete(bucket, "milk")
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end  
+end
